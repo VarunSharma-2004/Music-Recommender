c4923a2951c445c5d951fdde62b839bcd984a129
VarunSharma-2004 <varunsharma0410200@gmail.com>
2025-04-23 19:51:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Google Gemini API Key
-genai.configure(api_key="AIzaSyD-UsBAQiP5dDwE0ZISszCdWuLkta-xRX4")
+genai.configure(api_key=os.getenv("gemini_api"))
 
 # Google OAuth Setup
 client_id = "799230987280-slj2i30suidm19oa69k7uuf3fv8mghe1.apps.googleusercontent.com"
